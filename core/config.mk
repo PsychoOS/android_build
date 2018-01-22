f061ded6386210586e2b070fd7fa658e026c9dc9
Psycho Mods <nishsin26@gmail.com>
2018-01-22 11:47:05 +0530

    Update config.mk

--- core/config.mk
+++ core/config.mk
@@ -920,7 +920,7 @@ ifneq ($(TARGET_COPY_FILES_OVERRIDES),)
     PRODUCT_COPY_FILES := $(filter-out $(TARGET_COPY_FILES_OVERRIDES), $(PRODUCT_COPY_FILES))
 endif
 
-ifneq ($(PSYCHO_BUILD),)
+ifneq ($(CM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include vendor/psycho/sepolicy/sepolicy.mk)
